e7ec25c7b921f3ae7c807cb8fa6c0168bf63c56d
michaeldrotar <michaeldrotar@gmail.com>
2025-10-27 23:03:45 -0400

    Disable another dupe no-unused-vars rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -57,6 +57,7 @@ export default [
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
