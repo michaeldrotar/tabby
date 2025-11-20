132a977516437da10ed6bc4ecaa6f68329091aa0
michaeldrotar <michaeldrotar@gmail.com>
2025-11-20 02:30:26 -0500

    Fix extra favicons on dom body

--- pages/side-panel/src/Favicon.tsx
+++ pages/side-panel/src/Favicon.tsx
@@ -4,12 +4,8 @@ type FaviconProps = {
 } & Pick<React.JSX.IntrinsicElements['img'], 'alt' | 'className'>
 
 const Favicon = ({ pageUrl, size, ...imageProps }: FaviconProps) => {
-  // const pageUrl = "https://www.example.com"; // Replace with the actual page URL
   const src = `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(pageUrl || '')}&size=${size || '32'}`
 
-  const faviconImage = document.createElement('img')
-  faviconImage.src = src
-  document.body.appendChild(faviconImage) // Or append it to your desired element
   return (
     <img
       src={src}
