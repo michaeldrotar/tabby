b06be40cea0de65cfcdc631735ce145eb8ac8f08
michaeldrotar <michaeldrotar@gmail.com>
2025-12-02 01:28:15 -0500

    Fix import path

--- packages/module-manager/lib/processing/recover-feature.ts
+++ packages/module-manager/lib/processing/recover-feature.ts
@@ -1,7 +1,7 @@
 import { recoverModule } from './modules-handler.js'
 import { DEFAULT_CHOICES, RECOVER_CHOICE_QUESTION } from '../const.js'
 import { processSelection } from '../helpers/utils.js'
-import { archivePath } from 'lib/paths.js'
+import { archivePath } from '../paths.js'
 import { rimraf } from 'rimraf'
 import { existsSync, readdirSync } from 'node:fs'
 import { resolve } from 'node:path'
