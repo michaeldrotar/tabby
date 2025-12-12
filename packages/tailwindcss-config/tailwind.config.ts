7528bd2c83fa3b1f893749a813ea6a3c19494061
michaeldrotar <michaeldrotar@gmail.com>
2025-12-12 18:06:52 -0500

    Build themes from tailwind colors

--- packages/tailwindcss-config/tailwind.config.ts
+++ packages/tailwindcss-config/tailwind.config.ts
@@ -4,31 +4,36 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background) / <alpha-value>)',
-        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+        background: 'hsl(from var(--background) h s l / <alpha-value>)',
+        foreground: 'hsl(from var(--foreground) h s l / <alpha-value>)',
 
-        card: 'hsl(var(--card) / <alpha-value>)',
-        'card-foreground': 'hsl(var(--card-foreground) / <alpha-value>)',
+        card: 'hsl(from var(--card) h s l / <alpha-value>)',
+        'card-foreground':
+          'hsl(from var(--card-foreground) h s l / <alpha-value>)',
 
-        popover: 'hsl(var(--popover) / <alpha-value>)',
-        'popover-foreground': 'hsl(var(--popover-foreground) / <alpha-value>)',
+        popover: 'hsl(from var(--popover) h s l / <alpha-value>)',
+        'popover-foreground':
+          'hsl(from var(--popover-foreground) h s l / <alpha-value>)',
 
-        muted: 'hsl(var(--muted) / <alpha-value>)',
-        'muted-foreground': 'hsl(var(--muted-foreground) / <alpha-value>)',
+        muted: 'hsl(from var(--muted) h s l / <alpha-value>)',
+        'muted-foreground':
+          'hsl(from var(--muted-foreground) h s l / <alpha-value>)',
 
-        accent: 'hsl(var(--accent) / <alpha-value>)',
-        'accent-foreground': 'hsl(var(--accent-foreground) / <alpha-value>)',
+        accent: 'hsl(from var(--accent) h s l / <alpha-value>)',
+        'accent-foreground':
+          'hsl(from var(--accent-foreground) h s l / <alpha-value>)',
 
-        border: 'hsl(var(--border) / <alpha-value>)',
-        input: 'hsl(var(--input) / <alpha-value>)',
-        ring: 'hsl(var(--ring) / <alpha-value>)',
+        border: 'hsl(from var(--border) h s l / <alpha-value>)',
+        input: 'hsl(from var(--input) h s l / <alpha-value>)',
+        ring: 'hsl(from var(--ring) h s l / <alpha-value>)',
 
-        primary: 'hsl(var(--primary) / <alpha-value>)',
-        'primary-foreground': 'hsl(var(--primary-foreground) / <alpha-value>)',
+        primary: 'hsl(from var(--primary) h s l / <alpha-value>)',
+        'primary-foreground':
+          'hsl(from var(--primary-foreground) h s l / <alpha-value>)',
 
-        destructive: 'hsl(var(--destructive) / <alpha-value>)',
+        destructive: 'hsl(from var(--destructive) h s l / <alpha-value>)',
         'destructive-foreground':
-          'hsl(var(--destructive-foreground) / <alpha-value>)',
+          'hsl(from var(--destructive-foreground) h s l / <alpha-value>)',
       },
     },
   },
