4a338680efe4bc2521f0c82e033e5bb7384c0f4e
michaeldrotar <michaeldrotar@gmail.com>
2025-12-19 15:05:25 -0500

    Fix build issue with undefined chrome

--- packages/chrome/lib/window/browserWindowTypes.ts
+++ packages/chrome/lib/window/browserWindowTypes.ts
@@ -1,6 +1,5 @@
 /**
  * The types of chrome windows that are correct for browser windows.
  */
-export const browserWindowTypes: chrome.windows.WindowType[] = [
-  chrome.windows.WindowType.NORMAL,
-]
+export const browserWindowTypes: chrome.windows.WindowType[] =
+  typeof chrome !== 'undefined' ? [chrome.windows.WindowType.NORMAL] : []
